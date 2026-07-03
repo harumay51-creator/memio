0d05311b5d2b194b076fed54229b9c3483cd3926
harumay51-creator <harumay51@gmail.com>
2026-07-03 15:15:11 +0900

    Update tailwind config to use Pretendard

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'system-ui', '-apple-system', 'sans-serif'],
+        sans: ['Pretendard', 'Inter', 'system-ui', '-apple-system', 'sans-serif'],
       },
       colors: {
         'yuri': {
